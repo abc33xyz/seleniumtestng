75f408ce84891540a4179858aff5a05c4641eb19
Shamsul Bhuiyan <sbhuiyan2@georgebrown.ca>
2018-06-18 14:55:33 -0400

    newcommit

---
+++ src/test/java/com/w2a/utilities/Brokenlinks.java
@@ -0,0 +1,15 @@
+package com.w2a.utilities;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.*;
+
+public class Brokenlinks {
+
+	public static void findbrokenlinks(String url) {
+		
+		// get  all links 
+		List<WebElement> alllinks=new ArrayList<WebElement>();
+		
+	}
+}
--- src/test/java/com/w2a/utilities/constant.java
+++ src/test/java/com/w2a/utilities/constant.java
@@ -1,5 +1,14 @@
 package com.w2a.utilities;
 
-public class constant {
+import org.openqa.selenium.WebDriver;
 
+public class constant {
+	public static String baseUrl = "http://demo.guru99.com/selenium/newtours/";
+	public static String wikiUrl="https://en.wikipedia.org/wiki/Selenium_%28software%29";
+    public static String driverPath = "C:/Usb/";
+    public static String chrbipath="C:/Program Files (x86)/Google/Chrome/Application";
+    public static String amzUrl="https://www.amazon.ca/";
+    public static String gogUrl="http://google.ca";
+    public static WebDriver driver; 
+    public static String homePage = "http://www.zlti.com";
 }
